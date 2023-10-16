174b2b36111ac1b09bf5196518e5fb42cff13b17
Viver Val Bungag <viverbungag1@gmail.com>
2023-10-16 08:40:59 +0800

    New model structure

--- tokenizer_vars.py
+++ tokenizer_vars.py
@@ -12,10 +12,11 @@ def getTokenizer():
     # new_tokens = list(set(tempNewTokens) - set(tokenizer.get_vocab().keys()))
     # tokenizer.add_tokens(new_tokens)
     number_of_recipes = c.NUMBER_OF_RECIPES
+    vocab_size = c.VOCAB_SIZE
 
     tokenizer = ByteLevelBPETokenizer(
-        f"tokenizers/chef_tokenizer_{number_of_recipes}-vocab.json",
-        f"tokenizers/chef_tokenizer_{number_of_recipes}-merges.txt",
+        f"tokenizers/chef_tokenizer_{number_of_recipes}_vocab_{vocab_size}-vocab.json",
+        f"tokenizers/chef_tokenizer_{number_of_recipes}_vocab_{vocab_size}-merges.txt",
     )
 
     tokenizer.add_tokens(c.NEW_TOKENS_NO_SPACES)
